504d7acc1c0a9da008a1f5835ed6edf3938634d3
Your Name <you@example.com>
2025-05-05 09:10:59 -0300

    listview

--- ContaClick/app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ ContaClick/app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -1,38 +1,47 @@
 package com.ifsc.contaclick;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class
 MainActivity extends AppCompatActivity {
 
 
     int i=0;
+    String [] nomes= new String[]{"Mercurio", "Venus", "Terra", "Marte", "Jupter", "Saturno", "Urano", "Netuno"};
+    ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //associando objeto interface a variavel local
-        TextView tv=findViewById(R.id.textView);
-        tv.setText(getString(R.string.app_name));
 
-        Button b=findViewById(R.id.button);
-        b.setOnClickListener(new View.OnClickListener() {
+        //recuoerar listview
+        lv= findViewById(R.id.listview);
+
+
+        //adaptador
+        ArrayAdapter<String> a=new ArrayAdapter(
+                this,R.layout.item_lista,
+                R.id.textView,
+                nomes);
+
+        lv.setAdapter(a);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                tv.setText(Integer.toString(i));
-                i++;
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i= new Intent(getApplicationContext(), PlanetaActivity.class);
+                i.putExtra("nome",nomes[position]);
+
+                startActivity(i);
             }
         });
-
 
 
 
