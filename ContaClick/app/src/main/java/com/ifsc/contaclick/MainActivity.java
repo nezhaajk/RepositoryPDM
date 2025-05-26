6b66cd62350bd7e86a33c69af6ffd0a08024504e
gabriel <gabriel.t2007@aluno.ifsc.edu.br>
2025-05-26 08:23:01 -0300

    layout criado

--- ContaClick/app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ ContaClick/app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -15,23 +15,11 @@ public class
 MainActivity extends AppCompatActivity {
 
 
-    int i=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //associando objeto interface a variavel local
-        TextView tv=findViewById(R.id.textView);
-        tv.setText(getString(R.string.app_name));
 
-        Button b=findViewById(R.id.button);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tv.setText(Integer.toString(i));
-                i++;
-            }
-        });
 
 
 
