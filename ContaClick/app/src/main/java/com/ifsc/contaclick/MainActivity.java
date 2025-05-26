64d32da2501c9df440a4501f20d85d77e377db73
gabriel <gabriel.t2007@aluno.ifsc.edu.br>
2025-05-26 09:17:10 -0300

    bancodedados

--- ContaClick/app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ ContaClick/app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -1,28 +1,68 @@
 package com.ifsc.contaclick;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class
 MainActivity extends AppCompatActivity {
+
+
+    int i=0;
+    SQLiteDatabase db;
+    Button buttonInsere;
+    EditText editText;
+    ListView listView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db=openOrCreateDatabase("banco",MODE_PRIVATE,null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT, txt VARCHAR)");
+        buttonInsere=findViewById(R.id.buttonInsere);
+        editText=findViewById(R.id.editText);
+        listView=findViewById(R.id.listView);
+        buttonInsere.setOnClickListener(v->{
+            String msg =editText.getText().toString();
+            insertNota(msg);
+        });
+
+    }
+
+    public void listagemNotas(){
+        Cursor cursor= db.rawQuery("SELECT * FROM notas",null);
+        cursor.moveToFirst();
+        ArrayList<String> listaNotas=new ArrayList<String>();
+        while (!cursor.isAfterLast()){
+            int coluna=cursor.getColumnIndex("txt");
+            listaNotas.add(cursor.getString(coluna));
+            cursor.moveToNext();
+
+        }
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1
+        android.R.id.text1,
+                listaNotas);
+        listView.setAdapter(adapter);
 
 
+    }
 
 
-
+    public void insertNota(String txt){
+        //db.execSQL("INSERT INTO notas(txt) VALUES(" + " );");
+        ContentValues cv=new ContentValues();
+        cv.put("txt",txt);
+        db.insert("notas",null,cv);
     }
 }
