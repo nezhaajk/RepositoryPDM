7052374551f441ee441b8caf104853b278b38f84
Your Name <you@example.com>
2025-06-23 09:07:25 -0300

    laucher

--- ContaClick/app/src/main/java/com/ifsc/contaclick/AppAdapter.java
+++ ContaClick/app/src/main/java/com/ifsc/contaclick/AppAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -25,12 +26,31 @@ public class AppAdapter extends ArrayAdapter<ApplicationInfo> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         LayoutInflater layoutInflater= LayoutInflater.from(getContext());
         View v=layoutInflater.inflate(mresource,parent,false);
+
         ImageView imageView=v.findViewById(R.id.imageView);
         TextView tv=v.findViewById(R.id.textView);
+        LinearLayout ll=v.findViewById(R.id.linearlayout);
+
         ApplicationInfo appinfo=getItem(position);
 
         tv.setText(appinfo.loadLabel(getContext().getPackageManager()));
+        imageView.setImageDrawable(appinfo.loadIcon(getContext().getPackageManager()));
 
+        TextView tvpackgeName=new TextView(getContext());
+        tvpackgeName.setText(appinfo.packageName);
+        ll.addView(tvpackgeName);
+
+        tv=new TextView(getContext());
+        tv.setText(appinfo.className);
+        ll.addView(tv);
+
+        tv=new TextView(getContext());
+        tv.setText("datadir:"+appinfo.dataDir);
+        ll.addView(tv);
+
+        tv=new TextView(getContext());
+        tv.setText("scr:"+appinfo.sourceDir);
+        ll.addView(tv);
 
         return v;
     }
--- ContaClick/app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ ContaClick/app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -3,11 +3,13 @@ package com.ifsc.contaclick;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,20 +30,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        lv=findViewById(R.id.listView);
+
         pm=getPackageManager();
 
-        applicationInfoList=pm.getInstalledApplications(PackageManager.MATCH_ALL);
+       // applicationInfoList=pm.getInstalledApplications(PackageManager.MATCH_ALL);
+        Intent intentFilter =new Intent(Intent.ACTION_MAIN);
+        intentFilter.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> lRInfo= pm.queryIntentActivities(intentFilter,0);
+        applicationInfoList=new ArrayList<>();
+        for(ResolveInfo r:lRInfo){
+//            applicationInfoList.add(r.providerInfo.applicationInfo);
+            applicationInfoList.add(r.activityInfo.applicationInfo);
+        }
 
         AppAdapter appAdapter = new AppAdapter(this,R.layout.app_item, applicationInfoList);
         lv.setAdapter(appAdapter);
 
         lv.setOnItemClickListener((adapter, view, position, id) -> {
+
             ApplicationInfo applicationInfo=(ApplicationInfo) adapter.getItemAtPosition(position);
             Intent i =pm.getLaunchIntentForPackage(applicationInfo.packageName);
-
+            if(i!=null){
+                startActivity(i);
+            }
+            else{
+                Toast.makeText(getApplicationContext(),"App não lançável", Toast.LENGTH_LONG);
+            }
         });
     }
 
