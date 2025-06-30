7871a45a231d9cfd26df928de4ca5ba12324548d
Your Name <you@example.com>
2025-06-30 09:31:29 -0300

    imc

--- ContaClick/app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ ContaClick/app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -1,40 +1,49 @@
 package com.ifsc.contaclick;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class
-MainActivity extends AppCompatActivity {
+import com.ifsc.contaclick.MainActivityB;
+import com.ifsc.contaclick.R;
 
+import java.text.DecimalFormat;
 
+public class MainActivity extends AppCompatActivity {
     int i=0;
+    EditText edpeso,edaltura;
+    TextView tvresulado;
+    Button buttonCalcular;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("ciclo de vida","metodo onCreate");
         setContentView(R.layout.activity_main);
-        //associando objeto interface a variavel local
-        TextView tv=findViewById(R.id.textView);
-        tv.setText(getString(R.string.app_name));
+        edpeso=findViewById(R.id.edpeso);
+        edpeso.setText("80");
+        edaltura=findViewById(R.id.edaltura);
+        edaltura.setText("1.80");
 
-        Button b=findViewById(R.id.button);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tv.setText(Integer.toString(i));
-                i++;
-            }
+        buttonCalcular=findViewById(R.id.button);
+        //define um tratamento para o click do botão
+        buttonCalcular.setOnClickListener(v->{
+            Intent intent = new Intent(getApplicationContext(), MainActivityB.class);
+            Double peso= Double.parseDouble(edpeso.getText().toString());
+            Double altura= Double.parseDouble(edaltura.getText().toString());
+            //Definindi parametros para o bundle peso e altura
+            intent.putExtra("peso", peso);
+            intent.putExtra("altura", altura);
+
+            startActivity(intent);
         });
-
-
-
-
     }
+
 }
