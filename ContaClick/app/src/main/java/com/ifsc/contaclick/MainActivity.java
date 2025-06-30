3b3fad695dc2dd500d40402ffff3858644c98a9d
Your Name <you@example.com>
2025-06-30 08:52:18 -0300

    gps

--- ContaClick/app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ ContaClick/app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -1,5 +1,9 @@
 package com.ifsc.contaclick;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -7,34 +11,54 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class
-MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
+    LocationManager locationManager;
 
-    int i=0;
+    TextView tvLatidude, tvLongitude, tvStatus;
+
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //associando objeto interface a variavel local
-        TextView tv=findViewById(R.id.textView);
-        tv.setText(getString(R.string.app_name));
+        tvLatidude = findViewById(R.id.tvLatidude);
+        tvLongitude = findViewById(R.id.tvLongitude);
+        tvStatus = findViewById(R.id.tvStatus);
+        button = findViewById(R.id.button);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        Button b=findViewById(R.id.button);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tv.setText(Integer.toString(i));
-                i++;
-            }
+        button.setOnClickListener(v -> {
+            getLocalizacao();
         });
 
+    }
+    public void getLocalizacao() {
+
+        locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+    }
+    public boolean checaESolicitaPermissao(){
+        if (
+                ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED
+                ||
+                ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED
+        )
+
+        {
+
+        }
+    }
 
 
 
     }
-}
